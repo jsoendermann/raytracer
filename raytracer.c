817b0ed08c10c2d4eba15e7c934f8cfcb554f871
Jan Soendermann <jan.soendermann@googlemail.com>
2014-01-12 14:20:24 +0000

    Two spheres working

--- raytracer.c
+++ raytracer.c
@@ -13,30 +13,39 @@
 colour *trace(ray *r, sphere **spheres, int num_spheres, const int recursion_depth) {
     colour *c;
 
+    vector3 *min_distance = NULL;
+
     for (int i = 0; i < num_spheres; i++) {
         vector3 *intersect = intersect_sphere(r, spheres[i]);
         if (intersect != NULL) {
-            c = make_colour(230, 255, 255);
-        } else {
-            c = make_colour(0,0,0);
+            if (min_distance == NULL || distance(r->org, intersect) < distance(r->org, min_distance)) 
+                min_distance = intersect;
         }
     }
+
+    if (min_distance == NULL)
+        c = make_colour(255, 255, 255);
+     else 
+        c = make_colour(0,0,0);
+    
+
     return c;
 }
 
 int main(int argc, char **argv) {
     sphere *spheres[] = {
-        //make_sphere(make_vect(0, 40, -10-SCREEN_DISTANCE), 20),
-        make_sphere(make_vect(0, 0, -50-SCREEN_DISTANCE), 40)
+        make_sphere(make_vect(-10, -10, -75-SCREEN_DISTANCE), 30),
+        make_sphere(make_vect(20, 20, -100-SCREEN_DISTANCE), 40)
     };
 
     int num_spheres = sizeof(spheres)/sizeof(spheres[0]);
 
     colour* image[WIDTH * HEIGHT];
 
+    ray *r;
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            ray *r = make_ray(make_vect(0, 0, 0), make_normalised_vect(x-WIDTH/2, y-HEIGHT/2, -SCREEN_DISTANCE));
+            r = make_ray(make_vect(0, 0, 0), make_normalised_vect(x-WIDTH/2, y-HEIGHT/2, -SCREEN_DISTANCE));
 
             image[coords2index(x,y)] = trace(r, spheres, num_spheres, 0);
 
@@ -51,7 +60,9 @@ int main(int argc, char **argv) {
 }
 
 void write_image(colour **image) {
-    char *head = "P6\n 50 50\n255\n";
+    char head[20];
+    sprintf(head, "P6\n %d %d\n255\n", WIDTH, HEIGHT);
+
     FILE *f = fopen("image.ppm", "wb");
     if (!f) {
         printf("Unable to open file");
